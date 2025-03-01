b17fff9d193f347f8b8430042026c5e54d902de6
santhoshRS <raai.santhosh@gmail.com>
2025-03-01 23:35:16 +0530

    code changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // Ensures relative paths are used
+  base: '/tech-stack/', // Ensures relative paths are used
   plugins: [react()],
 })
